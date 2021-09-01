b60d7cd2da9170189181d5e4eed5d37a32afded8
Laura Aupert <aupert.laura@gmail.com>
2021-09-01 16:08:31 -0400

    Initial Commit

---
+++ src/main/java/com/oxydb/OxyDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.oxydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxyDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxyDatabaseApplication.class, args);
+	}
+
+}
